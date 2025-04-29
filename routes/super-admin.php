57f82a2d183141965fd0f7077be899e5ebd8f354
GeorginaAndrea <geomendezhdz@gmail.com>
2025-04-28 19:57:35 -0600

    Archivo rutas super-admin actualizado

--- routes/super-admin.php
+++ routes/super-admin.php
@@ -1,14 +1,9 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\HomeController;
-use App\Http\Controllers\Admin\AlumnoController;
-use App\Http\Controllers\Admin\CarreraController;
-use App\Http\Controllers\Admin\FacultadController;
-use App\Http\Controllers\Admin\GrupoController;
-use App\Http\Controllers\Admin\GrupoMateriaController;
-use App\Http\Controllers\Admin\KardexController;
-use App\Http\Controllers\Admin\ProfesorController;
-use App\Http\Controllers\Admin\MateriaController;
+use Illuminate\Support\Facades\Route;
+
 
     Route::get('/',[HomeController::class, 'index'])->name('admin.home');
+
+    
